62651aa3e7ba1fd51ceb2f7b7a42a0824df98f13
Joel M <joel@mariachi.local>
2017-08-10 23:32:41 -0700

    getTickerCap added to mock

--- src/testing/data.service.mock.ts
+++ src/testing/data.service.mock.ts
@@ -5,4 +5,8 @@ export class DataServiceMock {
   public getAll(): Observable<Object> {
     return Observable.of([]);
   }
+
+  public getTickerCap(): Observable<Object> {
+    return Observable.of([]);
+  }
 }
