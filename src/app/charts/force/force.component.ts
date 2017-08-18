1512a976b24c01b1f1f1078bb12c936add880ae5
Joel M <joel@mariachi.local>
2017-08-18 01:01:01 -0700

    working force chart

--- src/app/charts/force/force.component.ts
+++ src/app/charts/force/force.component.ts
@@ -1,32 +1,33 @@
 import {
   Component,
   Input,
-  OnInit,
+  Output,
   OnChanges,
   ViewEncapsulation,
   ViewChild,
-  ElementRef
+  ElementRef,
+  EventEmitter
 } from '@angular/core';
 import * as d3 from 'd3';
 
-import * as ChartData from '../../types/chart-data';
-// import DataPoint from '../../types/data-point';
+import * as Chart from '../../types/chart';
 
 @Component({
   selector: 'app-force',
   styleUrls: ['./force.component.scss'],
   encapsulation: ViewEncapsulation.None,
   template: `
-    <h3>Force Chart</h3>
-    <div class="d3-chart" #chart></div>
+    <div class="box">
+      <div class="d3-chart" #chart></div>
+    </div>
   `
 })
-export class ForceComponent implements OnInit {
+export class ForceComponent implements OnChanges {
   @ViewChild('chart') private chartContainer: ElementRef;
   @Input() data: Array<any>;
+  @Output() selectedNode: EventEmitter<any> = new EventEmitter();
 
-  private margin: any = { top: 20, bottom: 20, left: 20, right: 20 };
-  // private chart: any;
+  private margin: any = { top: 2, bottom: 2, left: 2, right: 2 };
   private svgWidth: number;
   private svgHeight: number;
   private chartWidth: number;
@@ -34,40 +35,134 @@ export class ForceComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {
-    this.createChart();
+  ngOnChanges() {
     if (this.data) {
-      // this.updateChart();
+      this.plotChart();
     }
   }
 
-  private builtData(): ChartData.Chart {
-    const range = 100;
+  private clicked = (d): void => {
+    this.selectedNode.emit(d);
+  };
+
+  private plotChart(): void {
+    const data = this.builtData();
+    const svg = this.drawCanvas();
+
+    const simulation: any = d3
+      .forceSimulation()
+      .force(
+        'link',
+        d3.forceLink().id(function(d: Chart.Node) {
+          return d.index.toString();
+        })
+      )
+      .force(
+        'collide',
+        d3.forceCollide((d: Chart.Node): number => d.r).iterations(10)
+      )
+      .force('charge', d3.forceManyBody())
+      .force(
+        'center',
+        d3.forceCenter(this.chartWidth / 2, this.chartHeight / 2)
+      )
+      .force('y', d3.forceY(0))
+      .force('x', d3.forceX(0));
+
+    const link = svg
+      .append('g')
+      .attr('class', 'links')
+      .selectAll('line')
+      .data(data.links)
+      .enter()
+      .append('line')
+      .attr('stroke', 'gray');
+
+    const dragstarted = d => {
+      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+      this.clicked(d);
+    };
+
+    const node = svg
+      .append('g')
+      .attr('class', 'nodes')
+      .selectAll('circle')
+      .data(data.nodes)
+      .enter()
+      .append('circle')
+      .attr('r', function(d: Chart.Node) {
+        return d.r;
+      })
+      .style('fill', (d: Chart.Node) => d.color)
+      .on('click', this.clicked)
+      .call(
+        d3
+          .drag()
+          .on('start', dragstarted)
+          .on('drag', dragged)
+          .on('end', dragended)
+      );
+
+    const ticked = function() {
+      link
+        .attr('x1', function(l: Chart.Link) {
+          return l.source.x;
+        })
+        .attr('y1', function(l: Chart.Link) {
+          return l.source.y;
+        })
+        .attr('x2', function(l: Chart.Link) {
+          return l.target.x;
+        })
+        .attr('y2', function(l: Chart.Link) {
+          return l.target.y;
+        });
+
+      node
+        .attr('cx', function(n: Chart.Node) {
+          return n.x;
+        })
+        .attr('cy', function(d: Chart.Node) {
+          return d.y;
+        });
+    };
+
+    simulation.nodes(data.nodes).on('tick', ticked);
+
+    simulation.force('link').links(data.links);
+
+    function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+  }
+
+  private builtData(): Chart.Force {
+    const max = Math.floor(this.data.length * 0.75);
     return {
-      nodes: d3.range(0, range).map(function(d) {
-        console.log(d);
-        return {
-          label: 'l' + d,
-          r: Math.floor(d3.randomUniform(8, 28)())
-        };
-      }),
-      links: d3.range(0, range).map(function() {
+      nodes: this.data,
+      links: d3.range(0, max).map(function(i): Chart.Link {
         const outLinks = {
-          source: Math.floor(d3.randomUniform(range)()),
-          target: Math.floor(d3.randomUniform(range)())
+          source: i,
+          target: Math.floor(d3.randomUniform(max / 5)())
         };
-        // console.log(outLinks);
         return outLinks;
       })
     };
   }
 
-  private createChart(): void {
-    // const margin: any = { top: 20, bottom: 20, left: 20, right: 20 };
-    // let chartWidth, chartHeight, width, height;
-    const element = this.chartContainer.nativeElement;
-    const data = this.builtData();
+  private drawCanvas(): any {
+    //d3.Selection<d3.BaseType, {}, null, undefined>
 
+    const element = this.chartContainer.nativeElement;
     this.svgWidth = element.offsetWidth - this.margin.left - this.margin.right;
     this.svgHeight =
       element.offsetHeight - this.margin.top - this.margin.bottom;
@@ -88,108 +183,6 @@ export class ForceComponent implements OnInit {
         'translate(' + [this.margin.left, this.margin.top] + ')'
       );
 
-    // console.log(data);
-    this.drawChart(data, svg);
-  }
-
-  private drawChart(data, svg) {
-    const simulation: any = d3
-      .forceSimulation()
-      .force(
-        'link',
-        d3.forceLink().id(function(d: ChartData.Point) {
-          // console.log(d);
-
-          return d.index.toString();
-        })
-      )
-      .force(
-        'collide',
-        d3
-          .forceCollide(function(d: ChartData.Point): number {
-            return d.r + 8;
-          })
-          .iterations(16)
-      )
-      .force('charge', d3.forceManyBody())
-      .force(
-        'center',
-        d3.forceCenter(this.chartWidth / 2, this.chartHeight / 2)
-      )
-      .force('y', d3.forceY(0))
-      .force('x', d3.forceX(0));
-
-    const link = svg
-      .append('g')
-      .attr('class', 'links')
-      .selectAll('line')
-      .data(data.links)
-      .enter()
-      .append('line')
-      .attr('stroke', 'black');
-
-    const node = svg
-      .append('g')
-      .attr('class', 'nodes')
-      .selectAll('circle')
-      .data(data.nodes)
-      .enter()
-      .append('circle')
-      .attr('r', function(d: ChartData.Point) {
-        // console.log(d);
-        return d.r;
-      })
-      .call(
-        d3
-          .drag()
-          .on('start', dragstarted)
-          .on('drag', dragged)
-          .on('end', dragended)
-      );
-
-    const ticked = function() {
-      link
-        .attr('x1', function(d: ChartData.Link) {
-          return d.source.x;
-        })
-        .attr('y1', function(d: ChartData.Link) {
-          return d.source.y;
-        })
-        .attr('x2', function(d: ChartData.Link) {
-          return d.target.x;
-        })
-        .attr('y2', function(d: ChartData.Link) {
-          return d.target.y;
-        });
-
-      node
-        .attr('cx', function(d: ChartData.Point) {
-          return d.x;
-        })
-        .attr('cy', function(d: ChartData.Point) {
-          return d.y;
-        });
-    };
-
-    simulation.nodes(data.nodes).on('tick', ticked);
-
-    simulation.force('link').links(data.links);
-
-    function dragstarted(d) {
-      if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-      d.fx = d.x;
-      d.fy = d.y;
-    }
-
-    function dragged(d) {
-      d.fx = d3.event.x;
-      d.fy = d3.event.y;
-    }
-
-    function dragended(d) {
-      if (!d3.event.active) simulation.alphaTarget(0);
-      d.fx = null;
-      d.fy = null;
-    }
+    return svg;
   }
 }
